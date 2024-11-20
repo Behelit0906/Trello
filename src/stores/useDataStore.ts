886a911881aedcd34184c5d6f5eec602338ac0a6
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-11-20 04:38:09 -0500

    feat: add functionality to select a board

--- src/stores/useDataStore.ts
+++ src/stores/useDataStore.ts
@@ -10,11 +10,39 @@ export const useDataStore = defineStore('dataStore', () => {
     return data ? JSON.parse(data) : defaultData;
   };
 
+  const saveDataToLocalStorage = <T>(key: string, data: T) => {
+    localStorage.setItem(key, JSON.stringify(data))
+  }
+
   const boards = ref<Board[]>(getDataFromLocalStorage<Board[]>('boards', dummyData.boards));
   const lists = ref<List[]>(getDataFromLocalStorage<List[]>('lists', dummyData.lists));
   const cards = ref<Card[]>(getDataFromLocalStorage<Card[]>('cards', dummyData.cards));
   const labels = ref<Label[]>(getDataFromLocalStorage<Label[]>('labels', dummyData.labels));
   const comments = ref<Comment[]>(getDataFromLocalStorage<Comment[]>('comments', dummyData.comments));
+  const selectedBoard = ref(localStorage.getItem('selectedBoard') ? localStorage.getItem('selectedBoard') : '')
+
+
+  const setSelectedBoard = (boarId: string) => {
+    selectedBoard.value = boarId
+    localStorage.setItem('selectedBoard', boarId)
+    const index = boards.value.findIndex(b => b.id === boarId);
+    
+    let validPosition = 0
+
+    for (let i = 0; i < boards.value.length; i++) {
+      if(!boards.value[i].favorite) {
+        validPosition = i
+        break
+      }
+    }
+
+    if (index && !boards.value[index].favorite) {
+      const board = boards.value.splice(index, 1)[0];
+      boards.value.splice(validPosition, 0, board);
+      saveDataToLocalStorage('boards', boards.value)
+    }
+  }
+  
 
   return {
     boards,
@@ -22,6 +50,8 @@ export const useDataStore = defineStore('dataStore', () => {
     cards,
     labels,
     comments,
+    selectedBoard,
+    setSelectedBoard
   };
   
 });
