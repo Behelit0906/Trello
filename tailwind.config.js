0550115182eae498cd4ec03a8123f75c44e9a68d
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-11-20 04:38:44 -0500

    chore: add new colors and background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ export default {
       },
       colors: {
         dark: '#1D2125',
+        textDark: '#9FADBC',
+        navbar: 'var(--dynamic-navbar-color)',
+        sidenavbar: 'var(--dynamic-side-navbar-color)',
+        dynamicTextColor: 'var(--dynamic-text-color)'
+      },
+      backgroundImage: {
+        1: "url('./src/assets/backgrounds/bg-1.webp')",
+        2: "url('./src/assets/backgrounds/bg-2.webp')",
+        3: "url('./src/assets/backgrounds/bg-3.webp')",
+        4: "url('./src/assets/backgrounds/bg-4.webp')"
       }
     },
   },
