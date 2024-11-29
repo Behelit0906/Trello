cbff00390df524db2add3c9414a07370c82b707e
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-11-29 04:35:42 -0500

    feat: add support to primitive values

--- src/composables/useStorage.ts
+++ src/composables/useStorage.ts
@@ -1,13 +1,23 @@
 import { ref, watch } from 'vue'
 
-export function useStorage<T>(key:string, initialValue:T) {
-
+export function useStorage<T>(key: string, initialValue: T) {
+  // Leer el valor del localStorage
   const storedValue = localStorage.getItem(key)
 
-  const data = ref<T>(storedValue ? JSON.parse(storedValue) : initialValue) 
+  // Determinar si debemos usar JSON.parse o no
+  const data = ref<T>(
+    storedValue
+      ? typeof initialValue === 'object'
+        ? JSON.parse(storedValue)
+        : (storedValue as unknown as T) // Si no es un objeto, simplemente lo usamos tal cual
+      : initialValue
+  )
 
+  // Observar cambios en el dato reactivo
   watch(data, () => {
-    localStorage.setItem(key, JSON.stringify(data.value))
+    const valueToStore =
+      typeof data.value === 'object' ? JSON.stringify(data.value) : data.value
+    localStorage.setItem(key, String(valueToStore))
   }, { deep: true })
 
   return data
